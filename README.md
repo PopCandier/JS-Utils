f093a69d9b08b7ff600ca006eca17d8c9a1b300b
Pop <824173124@qq.com>
2019-09-05 23:57:36 +0800

    修复分页工具的bug 1.页数计算超过初始计算量 2.配置参数不匹配

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 ### JavaScript 工具
 
 2019/9/5 easy-page.js 可以方便创建分页效果
+
+```html
+<script type="text/javascript" src="../js/jquery-3.3.1.min.js" ></script>
+<script type="text/javascript" src="../js/easy-page.js" ></script>
+<script type="application/javascript">
+		$(function(){
+			window.PAGE.initPage("","#page-box");
+		});
+</script>
+```
+
--- js/easy-page.js
+++ js/easy-page.js
@@ -12,15 +12,16 @@ $(document).ready(function(){
         option:{
             page:0,//当前多少页
             limit:10,//每页显示多少
-            count:10,//数据总数
+            count:126,//数据总数
             showP:3,//前面显示多少分页选项
-            showN:1//后面显示多少选项
+            showN:1,//后面显示多少选项
+            currP:1,//选中的当前页前面会有多少数目
+            currN:1//选中的当前页前面会有多少数目
         },
-        pageTotal:60,
+        pageTotal:0,
         context:null,//填充数据所在位置
         context0:null,//分页所在位置
         url:"",
-        cid:"",
         item:"",
         isInit:false,
         computePageTotal:function(option){
@@ -28,7 +29,7 @@ $(document).ready(function(){
             var c = option.count;
             var l = option.limit;
             var m = 0;
-            return (m=c%l)>1?c/l+m:c/l;
+            return (m=c%l)>0?Math.floor((c/l))+1:(c/l);
         },
         setOption:function(option){
         	var that = this;
@@ -42,12 +43,11 @@ $(document).ready(function(){
         	var that = this;
         	var o = option!=undefined?option:that.option;
             that.context=$(id);//数据填充容器
-            that.cid = cid;
             that.context0=$(cid);//分页填充容器
             that.url = url;
             that.item = item;
             that.setOption(o);
-            that.pageCount = that.computePageTotal(o);
+            that.pageTotal = that.computePageTotal(o);
             //可能存在标签自定义的初始化 todo
             that.pageItemInit();
             that.next();
@@ -60,6 +60,11 @@ $(document).ready(function(){
         	appendPanel:function(e){
         		this.panel+=e;
         	}
+        },
+        clear:function(){
+        	var that = this;
+        	that.pagePanel.panel="";
+        	that.context0.empty();
         },
         registerEvents:function(){
        		//我们只针对前进后退，点击页数进行跳转，实现原理在于判断是否是相应的className
@@ -81,11 +86,10 @@ $(document).ready(function(){
        			}
        			if(value.className.indexOf(itemConfig.normal.className)!=-1){
        				var jquery_value = $(value);
-       				$(value).click(function(e){that.jump(jquery_value.text);});
+       				$(value).click(function(e){that.jump(parseInt(jquery_value.text()));});
        			}
        		});
         },	
-        pageItems:[],
         pageItemConfig:{
         	total:{
         		className:'result',
@@ -150,19 +154,15 @@ $(document).ready(function(){
         	jump:{},
         	setText:function(target,txt){return target.replace(/#/g,txt);}
         },
-        hasPrevious:function(num){
-        	return num<=1?false:true;
-        },
-        hasNext:function(num){
-        	return num>=this.pageTotal?false:true;
-        },
+        hasPrevious:function(num){return num<=1?false:true;},
+        hasNext:function(num){return num>=this.pageTotal?false:true;},
         computerTimes:function(num){
         	var times=[];
         	var that = this;
         	var option = that.option;
         	var pageTotal = that.pageTotal;
-        	var begin = num-2;
-        	var end = num+2;
+        	var begin = num-option.currP;
+        	var end = num+option.currN;
     		/**
     		 * 如果按照当前传入的数字减去不越界的话，说明传入的值位于总页数的较为中间的位置，那么就保留递减的数字，全部输出
     		 * 如果越界，则就是从第一页开始
@@ -174,7 +174,7 @@ $(document).ready(function(){
     		return times;
         	
         },
-        move2num:function(num){
+        move2num:function(num0){
             var that = this;
             var option = that.option;
             var pageItem = that.pageItem;
@@ -182,117 +182,61 @@ $(document).ready(function(){
             var len = that.pageTotal;
             var showP = option.showP;
             var showN = option.showN;
+            var num = option.page;
+            that.clear();
             var total = pageItem.setText(pageItem.total,'共'+len+'页');
             pagePanel.appendPanel(total);
-            that.pageItems.push(total);
             var t = that.computerTimes(num);
             if(len<=showP){//总页数小于需要显示的页数，那么能显示多少就显示多少
                 if(that.hasPrevious(num)){//说明不是第一页，位于中间的位置
-                	var prev = pageItem.prev;
-                	pagePanel.appendPanel(prev);
-                	that.pageItems.push(prev);
+                	pagePanel.appendPanel(pageItem.prev);
                     for(var i = t[0],times=t[1];i<=times;i++){
-                        if(i==num){
-                        	var current = pageItem.setText(pageItem.current,i);
-                        	pagePanel.appendPanel(current);
-                        	that.pageItems.push(current);
-                        }else{
-                        	var normal = pageItem.setText(pageItem.normal,i);
-                        	pagePanel.appendPanel(normal);
-                        	that.pageItems.push(normal);
-                        }
+                        if(i==num){pagePanel.appendPanel(pageItem.setText(pageItem.current,i));}
+                        else{pagePanel.appendPanel(pageItem.setText(pageItem.normal,i));}
                     }
-                    if(that.hasNext(num)){
-                    	var next = pageItem.next;
-                    	pagePanel.appendPanel(next);
-                    	that.pageItems.push(next);
-                    }//说明也是中间的位置
+                    if(that.hasNext(num)){pagePanel.appendPanel(pageItem.next);}//说明也是中间的位置
                     else{/*已经到了最后一页，也没有显示的必要了*/}
                 }else{//是第一页，一定会有下一页的
                 	for(var i = t[0],times=t[1];i<=times;i++){
-                        if(i==num){
-                        	var current = pageItem.setText(pageItem.current,i);
-                        	pagePanel.appendPanel(current);
-                        	that.pageItems.push(current);
-                        }else{
-                        	var normal = pageItem.setText(pageItem.normal,i);
-                        	pagePanel.appendPanel(normal);
-                        	that.pageItems.push(normal);
-                        }
+                        if(i==num){pagePanel.appendPanel(pageItem.setText(pageItem.current,i));}
+                        else{pagePanel.appendPanel(pageItem.setText(pageItem.normal,i));}
                     }
-                    if(that.hasNext(num)){
-                    	var next = pageItem.next;
-                    	pagePanel.appendPanel(next);
-                    	that.pageItems.push(next);
-                    }
+                    if(that.hasNext(num)){pagePanel.appendPanel(pageItem.next);}
                 }
             }else{//超出了可显示部分。
                 if(that.hasPrevious(num)){
-                    var prev = pageItem.prev;
-                	pagePanel.appendPanel(prev);
-                	that.pageItems.push(prev);
+                	pagePanel.appendPanel(pageItem.prev);
+                	if(t[0]>1){
+                		pagePanel.appendPanel(pageItem.setText(pageItem.normal,1));
+                		pagePanel.appendPanel(pageItem.dian);
+                	}
                     for(var i = t[0],times=t[1];i<=times;i++){
-                        if(i==num){
-                        	var current = pageItem.setText(pageItem.current,i);
-                        	pagePanel.appendPanel(current);
-                        	that.pageItems.push(current);
-                        }else{
-                        	var normal = pageItem.setText(pageItem.normal,i);
-                        	pagePanel.appendPanel(normal);
-                        	that.pageItems.push(normal);
-                        }
+                        if(i==num){pagePanel.appendPanel(pageItem.setText(pageItem.current,i));}
+                        else{pagePanel.appendPanel(pageItem.setText(pageItem.normal,i));}
                     }
-                    if(len>(showP+showN)){
-                    	var dian = pageItem.dian;
-                    	pagePanel.appendPanel(dian);
-                    	that.pageItems.push(dian);
+//                  if((t[1]+option.showP)>len){/*todo*/}
+                    if(len!=(t[1])){
+                    	pagePanel.appendPanel(pageItem.dian);
+                    	//20 -2 18
+                    	for(var i = len,j=(len-showN+1);j<=i;j++){pagePanel.appendPanel(pageItem.setText(pageItem.normal,j));}
                     }
-                    for(var i = len,j=(len-showN);i>j;i--){
-                    	var normal = pageItem.setText(pageItem.normal,i);
-                    	pagePanel.appendPanel(normal);
-                    	that.pageItems.push(normal);
-                    }
-                    if(that.hasNext(num)){
-                    	var next = pageItem.next;
-                    	pagePanel.appendPanel(next);
-                    	that.pageItems.push(next);
-                    }
+                    if(that.hasNext(num)){pagePanel.appendPanel(pageItem.next);}
                 }else{
                 	for(var i = t[0],times=t[1];i<=times;i++){
-                        if(i==num){
-                        	var current = pageItem.setText(pageItem.current,i);
-                        	pagePanel.appendPanel(current);
-                        	that.pageItems.push(current);
-                        }else{
-                        	var normal = pageItem.setText(pageItem.normal,i);
-                        	pagePanel.appendPanel(normal);
-                        	that.pageItems.push(normal);
-                        }
+                        if(i==num){pagePanel.appendPanel(pageItem.setText(pageItem.current,i));}
+                        else{pagePanel.appendPanel(pageItem.setText(pageItem.normal,i));}
                     }
-                    if(len>(showP+showN)){
-                    	var dian = pageItem.dian;
-                    	pagePanel.appendPanel(dian);
-                    	that.pageItems.push(dian);
-                    }
-                    for(var i = len,j=(len-showN);i>j;i--){
-                    	var normal = pageItem.setText(pageItem.normal,i);
-                    	pagePanel.appendPanel(normal);
-                    	that.pageItems.push(normal);
-                    }
-                    if(that.hasNext(num)){
-                    	var next = pageItem.next;
-                    	pagePanel.appendPanel(next);
-                    	that.pageItems.push(next);
-                    }
+                    if(len>(showP+showN)){pagePanel.appendPanel(pageItem.dian);
+                    for(var i = len,j=(len-showN+1);j<=i;j++){pagePanel.appendPanel(pageItem.setText(pageItem.normal,j));}
+                    if(that.hasNext(num)){pagePanel.appendPanel(pageItem.next);}
                 }
-            }
-            that.context0.empty();
+            }}
             that.context0.append(pagePanel.panel);
             that.registerEvents();
-        },
+            },
         previous:function(){this.move2num(this.option.page--);},
         next:function(){this.move2num(this.option.page++);},
-        jump:function(num){this.move2num(num)},
+        jump:function(num){this.move2num(this.option.page=num)},
         active:function (suc,err){
             var that = this;
             var option = that.option;
